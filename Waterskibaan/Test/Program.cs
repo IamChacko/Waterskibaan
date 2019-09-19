4fc8cecbef35b06828db128ce830677b1f241f6c
Stefan <stefan.kieft@live.nl>
2019-09-19 14:07:56 +0200

    Opdracht#1

--- Waterskibaan/Test/Program.cs
+++ Waterskibaan/Test/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Waterskibaan
+namespace Test
 {
-    class Skies
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
--- Waterskibaan/Waterskibaan/Sporter.cs
+++ Waterskibaan/Waterskibaan/Sporter.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Waterskibaan
 {
-    class Class1
+    class Sporter : Zwemvest Zwe
     {
+        public int AantalRondenNogTeGaan { get; set; }
+        public Zwemvest Zwemvest { get; set; }
+        public Skies Skies { get; set; }
+        public Color KledingKleur { get; set }
+        public List<IMoves> Moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+            Moves = moves;
+            AantalRondenNogTeGaan = 0;
+        }
+
+        public int Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IMoves
+    {
+        int Move(); 
     }
 }
